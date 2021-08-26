40db071becaf472ed30ed99a3ed52663fe8c99c0
WellFilho <jwtlfilho15@hotmail.com>
2021-08-26 17:49:43 -0300

    alterei o RGB de 128 pra 127

--- index.py
+++ index.py
@@ -7,13 +7,14 @@ while True:
     conected, image = cam.read()
 
     grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    detecedFaces = classifier.detectMultiScale(grayImage, scaleFactor= 1.5, minSize=(100,100))
+    detecedFaces = classifier.detectMultiScale(
+        grayImage, scaleFactor=1.5, minSize=(100, 100))
 
     for (x, y, l, a) in detecedFaces:
-        
-        center = x + l // 2 , y + a // 2
+
+        center = x + l // 2, y + a // 2
         radius = round(l // 1.5)
-        cv2.circle(image ,center ,radius ,(0, 255, 128), 2)
+        cv2.circle(image, center, radius, (0, 255, 127), 2)
 
     cv2.imshow('Webcam', image)
     cv2.waitKey(1)
