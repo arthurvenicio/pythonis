1a653e38a5e55a350b77c61ebaa04c77511f6484
Arthur Venicio <stuks.midia@gmail.com>
2021-08-26 01:59:18 -0300

    now can detected human face

--- index.py
+++ index.py
@@ -1,3 +1,22 @@
 import cv2
-print(cv2.__version__)
-help(cv2.face)
+
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cam = cv2.VideoCapture(0)
+
+while True:
+    conected, image = cam.read()
+
+    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    detecedFaces = classifier.detectMultiScale(grayImage, scaleFactor= 1.5, minSize=(100,100))
+
+    for (x, y, l, a) in detecedFaces:
+        
+        center = x + l // 2 , y + a // 2
+        radius = round(l // 1.5)
+        cv2.circle(image ,center ,radius ,(0, 255, 128), 2)
+
+    cv2.imshow('Webcam', image)
+    cv2.waitKey(1)
+
+cam.realease()
+cv2.destroyAllWindows()
