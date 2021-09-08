09bc0278ffe6e73c98bb5a492b45e6aeaee5a233
WellFilho <jwtlfilho15@hotmail.com>
2021-09-08 20:26:14 -0300

    add requiriments

--- training.py
+++ training.py
@@ -2,7 +2,8 @@ import cv2
 import os
 import numpy as np
 
-eigen_face = cv2.face.EigenFaceRecognizer_create(num_components=50, threshold=2)
+eigen_face = cv2.face.EigenFaceRecognizer_create(
+    num_components=50, threshold=2)
 fisher_face = cv2.face.FisherFaceRecognizer_create()
 lbph = cv2.face.LBPHFaceRecognizer_create()
 
